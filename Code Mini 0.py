5e8c4560b365d5fc6fc8620e00dfdf3a23ab1f77
Silvio Carvalho <silvio.lc@uol.com.br>
2014-09-15 13:47:59 -0300

    Create Code Mini 0.py
    Code for peer review assignment # 0

---
+++ Code Mini 0.py
@@ -0,0 +1,7 @@
+# program template for mini-project 0
+
+# Modify the print statement according to 
+# the mini-project instructions
+
+# The code below was modified according structions:
+print "We want... a shrubbery!"
